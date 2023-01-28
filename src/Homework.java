0e7402eae0ab76eda1b23ee36873aef0e501be0e
CosmoBlack <soulker_mozyr@yahoo.com>
2023-01-28 10:55:54 +0200

    Task 1

--- src/Homework.java
+++ src/Homework.java
@@ -1,6 +1,27 @@
 public class Homework {
 
+
+    private static void highYearCalculation(int year) {
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 100 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+    }
     public static void main(String[] args) {
+
+        task1();
+
+    }
+
+    private static void task1() {
+
+        System.out.println("Задача 1");
+
+        int year = 2021;
+        highYearCalculation(year);
 
     }
 
